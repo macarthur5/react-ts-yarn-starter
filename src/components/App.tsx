576f3e50802e35ddd263d8bf2c00a8bc3c71110a
Martand <martand.chauhan@airtel.com>
2021-08-11 00:13:35 +0530

    beta changelist: tested for dynamic imports

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { Suspense, useState } from "react";
 import image1 from "./../assets/images/Image1.jpeg";
 import svgImage from "./../assets/vectors/folder.svg";
-import JSComponent from "./js-component";
+
+const Component = React.lazy(() => import("./js-component"));
 
 import "./App.scss";
 
 const App: React.FC = () => {
+  const [showComponent, setShowComponent] = useState<boolean>(false);
+
   return (
     <div className="image-div">
       <img src={image1} className="image1" alt="" width={350} height={200} />
       <img src={svgImage} alt="" width={200} height={200} />
-      <JSComponent />
+      <button
+        onClick={() => {
+          setShowComponent(true);
+        }}
+      >
+        Load Component
+      </button>
+
+      {showComponent ? (
+        <Suspense fallback={<div>Loading...</div>}>
+          <Component />
+        </Suspense>
+      ) : null}
     </div>
   );
 };
